9b0c8a509b9c858b8c155cec81d2ad22367df90e
chotung <charlie@allstar.gg>
2026-06-03 11:48:06 -0400

    routes for two controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Products API
+  resources :products, only: [:index, :show]
+
+  # Checkouts API
+  post "checkouts/quote", to: "checkouts#quote"
+  post "checkouts/complete", to: "checkouts#complete"
 end
